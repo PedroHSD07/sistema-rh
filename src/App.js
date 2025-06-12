a5bdf59b9fa1937d919dc08e80b8b2bbc41c4c79
andresqr19 <andre3s.sqr@gmail.com>
2025-06-11 22:40:27 -0300

    atualizações para conectar e tornar as telas funcionais

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import CadastroUsuario from './pages/CadastroUsuario';
+import { useState } from "react";
+import Layout from './pages/Layout';
+import Inicial from './pages/Inicial';
+import GestaoEmpresas from './pages/GestaoEmpresas';
+import CadastroPerfilUsuario from './pages/CadastroPerfilUsuario';
 
 function App() {
+  // Estado que controla qual página está sendo exibida
+  const [paginaAtiva, setPaginaAtiva] = useState('inicio');
+
+  // Função para renderizar a página correta
+  const renderizarPagina = () => {
+    switch (paginaAtiva) {
+      case 'inicio':
+        return <Inicial />;
+      case 'cadastro-empresa':
+        return <GestaoEmpresas />;
+      case 'cadastro-usuario':
+        return <CadastroPerfilUsuario />;
+      default:
+        return <Inicial />;
+    }
+  };
+
   return (
-    <CadastroUsuario />
+    <Layout paginaAtiva={paginaAtiva} setPaginaAtiva={setPaginaAtiva}>
+      {/* O conteúdo da página ativa será renderizado aqui */}
+      {renderizarPagina()}
+    </Layout>
   );
 }
 
